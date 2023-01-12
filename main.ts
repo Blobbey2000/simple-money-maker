91a38a86ad4a3a2fb75109812456145185cba6aa
Blobbey2000 <116673732+Blobbey2000@users.noreply.github.com>
2023-01-12 14:40:55 -0800

    Update pxt.json, main.blocks, main.ts, credit.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
-let howMuchMoney = 0
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    story.showPlayerChoices("Upgrade money per game update", "Cancel")
-    if (story.checkLastAnswer("Upgrade money per game update")) {
-        if (info.score() > howMuchMoney * 3) {
-            info.changeScoreBy(0 - howMuchMoney * 3)
-            howMuchMoney += 1
-            music.powerUp.play()
-        } else {
-            game.splash("You don't have enough money!")
-        }
-    }
-})
-game.onUpdateInterval(1000, function () {
-    info.changeScoreBy(1)
-    info.changeScoreBy(howMuchMoney)
-})
+let mySprite = sprites.create(img`
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    f f f f f f f f f f f f f f f f 
+    `, SpriteKind.Player)
+mySprite.sayText(Credits.credit("Blobbey"))
